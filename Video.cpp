8a49b76fa3892533de6267ff5a8a2595b93f261b
alvarogonmar <alvarogonmar0@gmail.com>
2025-06-02 19:32:58 -0600

    Implement placeholder methods for Video class: showAllVideosScore, addScore, getAverage, and getGenre

--- Video.cpp
+++ Video.cpp
@@ -3,3 +3,11 @@
 using namespace std;
 
 Video::Video(string type, string id, string title, string genre, int duration) : type(type), id(id), title(title), genre(genre), duration(duration) {}
+
+string Video::showAllVideosScore()
+{
+}
+
+double Video::addScore() {}
+double Video::getAverage() {}
+string Video::getGenre() {}
