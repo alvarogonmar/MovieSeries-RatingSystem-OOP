0af7b7039b82a5cea8c4004d3bd2a9aa8d16af01
alvarogonmar <alvarogonmar0@gmail.com>
2025-06-02 19:27:29 -0600

    Refactor Video class to improve access modifiers and ensure proper encapsulation

--- Video.h
+++ Video.h
@@ -8,4 +8,11 @@ private:
     string type, id, title, genre;
     int duration;
     vector<int> qualifications;
+
+public:
+    Video(string type, string id, string title, string genre, int duration);
+    string showAllVideosScore();
+    double addScore();
+    double getAverage();
+    string getGenre();
 };
