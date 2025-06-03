6ac4e52db1ba38d089e967b49d1856df6e2b3208
alvarogonmar <alvarogonmar0@gmail.com>
2025-06-02 19:30:35 -0600

    Implement Video class constructor and include necessary headers

---
+++ Video.cpp
@@ -0,0 +1,5 @@
+#include "Video.h"
+#include <iostream>
+using namespace std;
+
+Video::Video(string type, string id, string title, string genre, int duration) : type(type), id(id), title(title), genre(genre), duration(duration) {}
