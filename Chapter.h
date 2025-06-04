03b6be8283e3d60f3d09a5d1c67e8db55edbd46d
alvarogonmar <alvarogonmar0@gmail.com>
2025-06-03 22:17:45 -0600

    Refactor Chapter.h to include class definition and member variables

--- Chapter.h
+++ Chapter.h
@@ -1,3 +1,15 @@
 #include "Video.h"
 using namespace std;
 #include <iostream>
+
+class Chapter : public Video
+{
+protected:
+    int episodeNumber;
+    string serieTitle;
+
+public:
+    Chapter(string type, string id, string title, string genre, int duration, int episodeNumber, string serieTitle);
+    int getEpisodeNumber();
+    string getSerieTitle();
+};
