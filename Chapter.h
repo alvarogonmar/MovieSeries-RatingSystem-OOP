e1e7c3e71e97bfa32a83ae705963b423c5b781f9
alvarogonmar <alvarogonmar0@gmail.com>
2025-06-12 07:28:29 -0600

    Refactor Chapter class: remove unused print function and update access specifiers for get methods

--- Chapter.h
+++ Chapter.h
@@ -10,12 +10,11 @@ protected:
 
 public:
     Chapter(string type, string id, string title, string genre, string duration, string episodeNumber, string serieTitle);
-    void print() override;
     void printByScore() override;
     void printByGenre() override;
 
-    string getEpisodeNumber();
-    string getSerieTitle();
-    // friend ostream &operator<<(ostream &os, const Chapter &chapter); // Aqui tilice GitHub Copilot, al poner solamente  de ostream para adelante me marcaba error en
+    string getEpisodeNumber() const;
+    string getSerieTitle() const;
+    friend ostream &operator<<(ostream &os, const Chapter &chapter); // Aqui tilice GitHub Copilot, al poner solamente  de ostream para adelante me marcaba error en
     // " &operator" por lo que le pregunte y me dijo que tenia que poner el friend para que pudiera acceder a los datos privados o protegidos de Chapter
 };
