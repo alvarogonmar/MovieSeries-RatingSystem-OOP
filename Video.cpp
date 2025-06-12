a7a3bbd2a7144e70cdc64762d83c45b2a9fd7f05
alvarogonmar <alvarogonmar0@gmail.com>
2025-06-12 07:28:51 -0600

    Refactor Video class: restore operator<< implementation and update get methods to use const specifier

--- Video.cpp
+++ Video.cpp
@@ -6,18 +6,12 @@ using namespace std;
 
 Video::Video(string type, string id, string title, string genre, string duration) : type(type), id(id), title(title), genre(genre), duration(duration) {}
 
-// ostream &operator<<(ostream &os, const Video &video)
-// {
-//     video.print(os); // ChatGPT
-//     return os;
-// }
-
 void Video::addScore(double score)
 {
     qualifications.push_back(score);
 }
 
-double Video::getAverage()
+double Video::getAverage() const
 {
     if (qualifications.empty())
         return -1;
@@ -30,8 +24,8 @@ double Video::getAverage()
                                         // donde checa si el vector quealifications esta vacio, si si, retorna - 1
 }
 
-string Video::getType() { return type; }
-string Video::getGenre() { return genre; }
-string Video::getId() { return id; }
-string Video::getTitle() { return title; }
-string Video::getDuration() { return duration; }
+string Video::getType() const { return type; }
+string Video::getGenre() const { return genre; }
+string Video::getId() const { return id; }
+string Video::getTitle() const { return title; }
+string Video::getDuration() const { return duration; }
